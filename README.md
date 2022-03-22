754f66b6e7395459e320c7f037514c0c6c59ce32
Odunola Jenrola <jenrola2292@gmail.com>
2022-03-22 16:06:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# 🤖An Anime Webscrapper built with Python
+### This scrapper retrieves details of top Anime from the [_Animelist_](https://myanimelist.net/topanime.php "Animelist") Website
+
+
+# 🤌Background
+### This is part of a beginner level project i began recently. My goal was to perform analysis on various anime to figure out what distinct feature made a particular anime more popular than others. Data available was not sufficient for my task so i decided to write a webscraper that creates a useful dataset for me. 
+
+# Overview
+## The document _anime_scrapper.py_ contains the scrapper
+### This code utilises the popular python webscraping libraries Requests and Beautiful Soup. Ensure that you have the libraries in the _requirements.txt_ to run the program well
+### after running the program,  a csv file containing the dataset is created in the current directory
+
+# How to use
+
+### To run the program;
+```
+# number of rows required indicates the number of top animes to be scrapped
+animescrape("number of rows required")
+```
+
+## Note: Ensure you rename your csv file after you run the program as it could be overwritten
+
+## Enjoy 😊 
+
+
+
---
+++ anime.py
@@ -0,0 +1,122 @@
+import requests
+import bs4
+import csv
+
+#creating header
+columns = [
+    "Name",
+    "Type",
+    "Episode",
+    "Aired",
+    "Premiered",
+    "Producers",
+    "Studios",
+    "Source",
+    "Genre",
+    "Demographic",
+    "Theme",
+    "Duration",
+    "Rating",
+    "Score",
+    "Ranking",
+    "Popularity",
+    "Members"
+]
+"""
+fields = [
+    "Type",
+    "Episodes",
+    "Aired",
+    "Premiered",
+    "Producers",
+    "Studios",
+    "Source",
+    "Genre",
+    "Demographic",
+    "Theme",
+    "Duration",
+    "Rating",
+]
+"""
+fields = columns[1:12]
+
+#creating the file
+file = open("anime_data.csv",mode = "w")
+writer = csv.writer(file)
+writer.writerow(columns)
+
+
+
+
+#get name
+def get_name(page):
+    name = page.find("h1",class_ = "title-name").text.strip()
+    return [name]
+
+#get type to rating
+def get_type_to_rating(page,field):
+    left = page.find("div",class_ = "leftside")
+    divs = left.findAll("div",class_ = "spaceit_pad")
+    #get first and last
+    for i in divs:
+        if "Type" in i.text:
+            start = divs.index(i)
+        if "Rating" in i.text:
+            end = divs.index(i) +1
+    divs = divs[start:end]
+    values = {}
+    values_list = []
+    for div in divs:
+        values[div.text.split(":")[0].strip()] = div.text.split(":")[1].strip()
+    for i in field:
+        if i in values.keys():
+            values_list.append(values[i])
+        else:
+            values_list.append("empty")
+    return values_list
+
+#get score,ranking and popularity
+def get_ScoreRankingPopularity(page):
+    #get score
+    score_to_last = []
+    score = page.find("div",class_ = "score-label").text
+    score_to_last.append(score)
+    #get ranking
+    left = page.find("div",class_ = "leftside")
+    divs = left.findAll("div",class_ = "spaceit_pad")[-3:]
+    for i in divs:
+        try:
+            score_to_last.append(i.text.split("#")[1].strip())
+        except:
+            score_to_last.append(i.text.split(":")[1].strip())
+    return score_to_last
+
+def anime_scrape(row):
+    count = 0
+    number = 0
+    while number <=row:
+        try:
+            #get each page
+            browser = requests.get(r"https://myanimelist.net/topanime.php?limit={}".format(number))
+            page = bs4.BeautifulSoup(browser.text,"html.parser")
+            #get the table links for each page
+            table_row = page.findAll("tr")[1:]
+            row_links = [row.find("a")["href"] for row in table_row]
+            #enter each linkA
+            for link in row_links:
+                count = count + 1
+                print(f"number {count}")
+                browser = requests.get(link)
+                page = bs4.BeautifulSoup(browser.text,"html.parser")
+                total = get_name(page) + get_type_to_rating(page,fields) + get_ScoreRankingPopularity(page)
+                writer.writerow(total)
+            
+        except:
+            print(f"error at number {count}")
+            count = count + 1
+        number+=50
+    print("saving file...")
+    file.close()
+    print("Done")
+
+anime_scrape(50)
